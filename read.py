1f3f40f92e944ea0ce35a62d213ca021213f5af9
glenhartwig <glenhartwi@gmail.com>
2014-02-19 20:56:22 +0100

    added settings.json and automate indexing

--- read.py
+++ read.py
@@ -1,20 +1,33 @@
 import time
 import couchdb
+import pycurl
+import os, sys
+import json
 
-#settings
-server_url = 'http://localhost:5984/'
-dbname = 'temperature'
+os.chdir(os.path.dirname(sys.argv[0]))
 
-couch = couchdb.Server(server_url)
+data = json.load(open("settings.json"))
+host = str(data["database"]["host"])
+port = str(data["database"]["port"])
+user = str(data["database"]["user"])
+password = str(data["database"]["password"])
+dbname = str(data["database"]["name"])
+sensors = data["sensors"]
+
+c = pycurl.Curl()
+c.setopt(c.URL, 'http://' + user + ':' + password + '@' + host + ':' + port + '/' + dbname + '/_design/temperature/_view/time') 
+c.setopt(c.WRITEFUNCTION, lambda x: None)
+
+couch = couchdb.Server('http://' + host + ':' + port)
 db = couch[dbname]
-sensorids = ["28-0000053b9dd6"]
-for sensor in sensorids:
-	temperatures = []
-	while True:
+
+while True:
+	for sensor in sensors:
+		print sensor["id"]
 		text = ''
 		# Read until the check is successful
 		while text.split("\n")[0].find("YES") == -1:
-			tfile = open("/sys/bus/w1/devices/%s/w1_slave" % sensor)
+			tfile = open("/sys/bus/w1/devices/%s/w1_slave" % sensor["id"])
 			# Read all of the text in the file.
 			text = tfile.read()
 			# Close the file now that the text has been read.
@@ -28,7 +41,7 @@ for sensor in sensorids:
 		temperature = float(temperaturedata[2:])
 		# Put the decimal point in the right place and display it.
 		temperature = temperature / 1000
-		document = {"sensor_id":sensor,"temperature":temperature,"time":int(round(time.time()*1000))}
+		document = {"sensor_id":sensor["id"],"temperature":temperature,"time":int(round(time.time()*1000))}
 		db.save(document)
-		temperatures.append(temperature)
-		time.sleep(25)
+	c.perform()
+	time.sleep(25)
