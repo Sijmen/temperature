7d5a29cc62c550c044bb9026cacf4ad309761ab4
glenhartwig <glenhartwi@gmail.com>
2014-02-20 02:52:04 +0100

    update multiple sensors symultaneous uses treading instead of thread now

--- read.py
+++ read.py
@@ -1,9 +1,9 @@
-import time, datetime
+import time
 import couchdb
 import pycurl
 import os, sys
 import json
-import thread
+import threading
 
 if os.path.dirname(sys.argv[0]):
 	os.chdir(os.path.dirname(sys.argv[0]))
@@ -23,29 +23,30 @@ c.setopt(c.WRITEFUNCTION, lambda x: None)
 couch = couchdb.Server('http://%s:%s' % (host,port))
 db = couch[dbname]
 
-def update_temperature(threadName):
-	for sensor in sensors:
-		text = ''
-		# Read until the check is successful
-		while text.split("\n")[0].find("YES") == -1:
-			tfile = open("/sys/bus/w1/devices/%s/w1_slave" % sensor["id"])
-			# Read all of the text in the file.
-			text = tfile.read()
-			# Close the file now that the text has been read.
-			tfile.close()
-			time.sleep(1)
-		# Split the text with new lines (\n) and select the second line.
-		secondline = text.split("\n")[1]
-		# Split the line into words, referring to the spaces, and select the 10th word (counting from 0).
-		temperaturedata = secondline.split(" ")[9]
-		# The first two characters are "t=", so get rid of those and convert the temperature from a string to a number.
-		temperature = float(temperaturedata[2:])
-		# Put the decimal point in the right place and display it.
-		temperature = temperature / 1000
-		document = {"sensor_id":sensor["id"],"temperature":temperature,"time":int(round(time.time()*1000))}
-		db.save(document)
-	c.perform()	
+
+def update_temperature(sensorId):
+	text = ''
+	# Read until the check is successful
+	while text.split("\n")[0].find("YES") == -1:
+		tfile = open("/sys/bus/w1/devices/%s/w1_slave" % sensorId)
+		# Read all of the text in the file.
+		text = tfile.read()
+		# Close the file now that the text has been read.
+		tfile.close()
+	# Split the text with new lines (\n) and select the second line.
+	secondline = text.split("\n")[1]
+	# Split the line into words, referring to the spaces, and select the 10th word (counting from 0).
+	temperaturedata = secondline.split(" ")[9]
+	# The first two characters are "t=", so get rid of those and convert the temperature from a string to a number.
+	temperature = float(temperaturedata[2:])
+	# Put the decimal point in the right place and display it.
+	temperature = temperature / 1000
+	document = {"sensor_id":sensorId,"temperature":temperature,"time":int(round(time.time()*1000))}
+	db.save(document)
+	if threading.activeCount() == 2:
+		c.perform()
 
 while True:
-	thread.start_new_thread(update_temperature,("temperature",))
+	for sensor in sensors:
+		threading.Thread(target=update_temperature, args=(sensor["id"],)).start()
 	time.sleep(30)
