144f90bdee1afc531301f933aaf8fb9aec5c3347
Sijmen <sijmen.hoogendijk@gmail.com>
2014-02-24 10:13:10 +0100

    refactored app.js

--- _attachments/js/local/app.js
+++ _attachments/js/local/app.js
@@ -1,14 +1,42 @@
 $oDB = $.couch.db('temperature');
-var tempData = new TemperatureDataProvider($oDB,{
-	filter:'temperature/measurement',
-	view:"temperature",
-	sensor_id:"28-0000053bccc5"
-});
-var graph = new MultiGraphTile('#graph',tempData,{
-	range:{
-		min:(new Date()).getTime()-24*60*60*1000,
+//One stream to rule them all!
+var dbStream = Rx.Observable.fromCouchDB($oDB);
+
+/*GENERAL OPTIONS*/
+var range24hours = {
+		min:(new Date()).getTime()-24*3600*1000,
 		max:(new Date()).getTime()
-	},
+	};
+
+var range60minutes = {
+		min:(new Date()).getTime()-3600*1000,
+		max:(new Date()).getTime()
+	};
+
+var baseOptions = {
+	view:'time',
+	designDoc:"temperature"
+};
+var baseOptions24h = $.extend({range:range24hours},baseOptions);
+var baseOptions60m = $.extend({range:range60minutes},baseOptions);
+
+//Get the historic values
+var viewTemperature_24h = Rx.Observable.fromCouchDBView($oDB,$.extend({device:'28-0000053bccc5'},baseOptions24h));
+var viewTemperature_60m = Rx.Observable.fromCouchDBView($oDB,$.extend({device:'28-0000053bccc5'},baseOptions60m));
+//Create the live stream for one sensor/device
+var temperatureStream = dbStream.filter(function(doc){return doc.sensor_id == "28-0000053bccc5";});
+// Helper function to map values of db to values to graph.
+function mapTemp(doc){
+	console.log(doc);
+	return {
+		x:Math.round(doc.time/1000),
+		y:doc.temperature
+	};
+}
+
+// Create a 24hour graph
+var graph = new GraphTile('#graph',viewTemperature_24h.concat(temperatureStream).map(mapTemp),{
+	range:range24hours,
 	serie:{
 		name : 'Temperatuur',
 		data : [],
@@ -16,11 +44,7 @@ var graph = new MultiGraphTile('#graph',tempData,{
 	},
 	name:'24 uur'
 });
-// graph.vAddSerie(tempData.oFilter({graph:{y:"temperature",x:"time"}}),{
-// 	range:{
-// 		min:(new Date()).getTime()-24*60*60*1000,
-// 		max:(new Date()).getTime()
-// 	},
+// graph.vAddSerie(viewTemperature_24h_2,{
 // 	serie:{
 // 		name : 'Temperatuur',
 // 		data : [],
@@ -28,20 +52,35 @@ var graph = new MultiGraphTile('#graph',tempData,{
 // 	}
 // });
 graph.vSetSmooth(12);
+graph.vRender();
 
 
-var graph2 = new LiveGraphTile('#graph2',tempData,{
-	range:{
-		min:(new Date()).getTime()-10*60*1000,
-		max:(new Date()).getTime()
-	},
+var graph2 = new GraphTile('#graph2',viewTemperature_60m.map(mapTemp),{
+	range:range60minutes,
 	serie:{
 		name : 'Temperatuur',
 		data : [],
 		color : '#c05020'
 	},
-	name:'5 minuten'
+	name:'60 minuten'
 });
-
-var number = new LiveKnobNumberTile('#number1',tempData,{name:"Laatste waarde"});
+graph2.vRender();
+var number = new LiveKnobNumberTile('#number1',temperatureStream.map(function(doc){return doc.temperature;}),{name:"Laatste waarde",max:30,step:0.5});
 number.vSetPostText(' &deg;C');
+
+var index = new LiveKnobNumberTile('#indexing',ActiveTasksDataProvider.map(function(data){
+	var activeTask = null;
+	$.each(data,function(key,task){
+		if(task.type == "indexer")
+			if(activeTask === null)
+				activeTask = task;
+			activeTask = task.progress < activeTask.progress ? task : activeTask ;
+	});
+	if(activeTask === null)
+		return 100;
+	return parseFloat(((activeTask.changes_done/activeTask.total_changes)*100).toFixed(1));
+}),{
+	max:100,
+	step:0.1
+});
+index.vSetPostText(' %');
